321029102576b056dc4ec027ceee7fdc04a17814
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2022-04-27 11:25:53 +0800

    后端接口完成构建
    采用Express框架
    尝试用一种很好看的console.log()方式输出接口的声明信息

---
+++ daily-api/src/app.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// 创建 application/json 编码解析
+const bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+// 定义Node Express服务器
+var server = app.listen(6666, 'localhost', function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Server running at http://%s:%s", host, port)
+})
+
+// 设置允许跨域访问
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+
+
+
+/**
+ * @API API主页
+ * @method GET,POST
+ * @return {}
+ */
+app.all('/', function (req, res) {
+    res.json("Welcome to 'Daily Party History' ! Here is our Back-End's apis.");
+    console.log('API home page visited.');
+})
+
+
+
+/**
+ * @API 获取接口信息（名称、版本号、仓库地址）
+ * @method GET
+ * @param null
+ * @return {JSONP}
+ */
+app.get('/info', function (req, res) {
+    ProjectName = "DailyPartyHistory"; // 项目名称
+    Version = "v1.0.0"; // 版本号
+    Repository = "Github: https://github.com/gdmuna/DailyPartyHistory"; // 仓库地址
+    const printInfo = "console.log(" +
+                                "`%c " + ProjectName + " %c " + Version + " %c " + Repository + "`, " +
+                                "'background: #ff6d6d; padding: 4px; border-radius: 3px 0 0 3px; color: #fff', " +
+                                "'padding: 3px; border-radius: 0 3px 3px 0; color: #409eff; border: 1px solid #d9ecff; background-color: #ecf5ff;', " +
+                                "'background: transparent'" +
+                                ");";
+    res.send(printInfo);
+    console.log('Output api info successfully.');
+})
+
+
+/**
+ * @API 获取“今日党史”内容
+ * @method POST
+ * @param {month: MONTH,day: DAY}
+ * @return {id: 一年中的第几天, Date: 月-日, content: 党史内容}
+ */
+app.post('/DailyContent', jsonParser, function (req, res) {
+
+    date_m = req.body.month; // 获取前端传回的月份
+    date_d = req.body.day; // 获取前端传回的日期
+
+    fs.readFile('./db/PartyHistory-m' + date_m + '.json', 'utf-8', function (err, data) {
+        x = JSON.parse(data); // 解析读取到的json文件（转换为JSON对象）
+
+        var response = x.word[date_d - 1]; // 获取对应日期的党史内容
+        res.json(response); // 以JSON字符串的格式返回给前端
+
+        console.log(response);
+    })
+})
