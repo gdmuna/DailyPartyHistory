19810833a19d70ab9143110f292ae770c57e53e9
Seele.Clover <37256067+seeleclover@users.noreply.github.com>
2022-05-13 00:16:26 +0800

    v2.0.0 版本上线
    1.新增了随机团史的接口。
    2.修改了“今日党史”的接口名称和部分变量的命名。

--- daily-api/src/app.js
+++ daily-api/src/app.js
@@ -20,14 +20,14 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Content-Type', 'application/json;charset=utf-8');
     next();
-});
+})
 
 
 
 /**
  * @API API主页
  * @method GET,POST
- * @return {}
+ * @return <text/html>
  */
 app.all('/', function (req, res) {
     //res.json("Welcome to 'Daily Party History' ! Here is our Back-End's apis.");
@@ -46,7 +46,7 @@ app.all('/', function (req, res) {
  */
 app.get('/info', function (req, res) {
     ProjectName = "DailyPartyHistory"; // 项目名称
-    Version = "v1.0.0"; // 版本号
+    Version = "v2.0.0"; // 版本号
     Repository = "Github: https://github.com/gdmuna/DailyPartyHistory"; // 仓库地址
     const printInfo = "console.log(" +
                                 "`%c " + ProjectName + " %c " + Version + " %c " + Repository + "`, " +
@@ -59,13 +59,14 @@ app.get('/info', function (req, res) {
 })
 
 
+
 /**
  * @API 获取“今日党史”内容
  * @method POST
- * @param {month: MONTH,day: DAY}
+ * @param {month: MONTH, day: DAY}
  * @return {id: 一年中的第几天, Date: 月-日, content: 党史内容}
  */
-app.post('/DailyContent', jsonParser, function (req, res) {
+app.post('/cpcContent', jsonParser, function (req, res) {
 
     date_m = req.body.month; // 获取前端传回的月份
     date_d = req.body.day; // 获取前端传回的日期
@@ -74,6 +75,29 @@ app.post('/DailyContent', jsonParser, function (req, res) {
         x = JSON.parse(data); // 解析读取到的json文件（转换为JSON对象）
 
         var response = x.word[date_d - 1]; // 获取对应日期的党史内容
+        res.json(response); // 以JSON字符串的格式返回给前端
+
+        console.log(response);
+    })
+})
+
+
+
+/**
+ * @API 获取随机的团史内容
+ * @method GET,POST
+ * @return {title: 团史标题, content: 团史内容}
+ */
+app.all('/ccylContent', function (req, res) {
+    fs.readFile('./db/ccylHistory.json', 'utf-8', function (err, data) {
+        x = JSON.parse(data); // 解析读取到的json文件（转换为JSON对象）
+        n = Math.floor(Math.random() * x.word.length) // 获得一个随机数n
+
+        // 获取随机的团史内容
+        var response = {
+            title: x.word[n].title,
+            content: x.word[n].content
+        };
         res.json(response); // 以JSON字符串的格式返回给前端
 
         console.log(response);
